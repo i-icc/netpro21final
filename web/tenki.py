2d62a1a6eccbab586d67e13060823986e8d5ba32
新田一斗 <nittaichito@shintaichitonoMacBook-Pro.local>
2021-06-21 11:01:42 +0900

    create dataset

--- web/tenki.py
+++ web/tenki.py
@@ -1,12 +1,17 @@
 import requests
+from data import DataSet
 
-def main():
+def get_w(code):
     url_items = 'https://weather.tsukumijima.net/api/forecast'
     item_data = {
-        'city':400040
+        'city':code
     }
     r = requests.get(url_items, json=item_data)
-    print(r.json())
+    return r.json()
+
+def main():
+    code = DataSet.city["稚内"]
+    print(get_w(code))
 
 
 if __name__=="__main__":
