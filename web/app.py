6253a1a1497abf6f62ee44227a03ee324d96d200
新田一斗 <nittaichito@dot1xs4425.14-192-19.wlan.ms.dendai.ac.jp>
2021-06-23 15:28:28 +0900

    CREATE ERROR and RESULT

--- web/app.py
+++ web/app.py
@@ -1,22 +1,24 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from data import DataSet
-from tenki import get_w
+from tenki import get_w, processing_json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    data = DataSet
-    return render_template('index.html',data=data)
+    return render_template('index.html', data=DataSet)
+
 
 @app.route('/result', methods=['GET'])
 def result():
     city = request.args.get('city')
-    data = DataSet
-    if city not in data.city:
+    if city not in DataSet.city:
         return render_template('error.html')
-    j = get_w(data.city[city])
-    return render_template('result.html',q=j)
+    j = get_w(DataSet.city[city])
+    j = processing_json(j)
+    return render_template('result.html', q=j)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- web/tenki.py
+++ web/tenki.py
@@ -1,17 +1,37 @@
 import requests
 from data import DataSet
 
+
 def get_w(code):
     url_items = 'https://weather.tsukumijima.net/api/forecast'
     item_data = {
-        'city':code
+        'city': code
     }
     r = requests.get(url_items, json=item_data)
     return r.json()
+
+
+def processing_json(r):
+    result = {}
+    result["locate"] = r["location"]["prefecture"] + r["location"]["city"]
+    today = r["forecasts"][0]
+    result["weather"] = today["telop"]
+    result["wave"] = today["detail"]["wave"]
+    result["temp"] = today["temperature"]["max"]["celsius"]
+    result["moi"] = today["temperature"]["max"]["fahrenheit"]
+    result["降水確率"] = today["chanceOfRain"]
+    result["image_svg"] = today["image"]["url"]
+    return result
+
+
+def choice_close():
+    pass
+
 
 def main():
     code = DataSet.city["稚内"]
     print(get_w(code))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
