2667dfb3fd65c40eb57b3e5125913a1dfcc05ede
新田一斗 <nittaichito@dot1xs4425.14-192-19.wlan.ms.dendai.ac.jp>
2021-06-23 16:21:43 +0900

    a

--- main/tk.py
+++ main/tk.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import socket
 import time
 
+
 class App(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
@@ -17,20 +18,24 @@ class App(tk.Tk):
         self.main_frame = tk.Frame()
         self.main_frame.grid(row=0, column=0, sticky="nsew")
         # タイトル
-        self.titleLabel = tk.Label(self.main_frame, text="アプリ名", font=('Helvetica', '35'))
+        self.titleLabel = tk.Label(
+            self.main_frame, text="アプリ名", font=('Helvetica', '35'))
         self.titleLabel.pack()
         # 接続ボタン
-        self.changePageButton = tk.Button(self.main_frame, text="サーバーに接続する", command=lambda : self.connect())
+        self.changePageButton = tk.Button(
+            self.main_frame, text="サーバーに接続する", command=lambda: self.connect())
         self.changePageButton.pack()
 
         # エラー画面
         self.error = tk.Frame()
         self.error.grid(row=0, column=0, sticky="nsew")
         # タイトル
-        self.titleLabel = tk.Label(self.error, text="エラーが生じました", font=('Helvetica', '35'))
+        self.titleLabel = tk.Label(
+            self.error, text="エラーが生じました", font=('Helvetica', '35'))
         self.titleLabel.pack()
         # ボタン
-        self.changePageButton = tk.Button(self.error, text="ホームへ戻る", command=lambda : self.changePage(self.main_frame))
+        self.changePageButton = tk.Button(
+            self.error, text="ホームへ戻る", command=lambda: self.changePage(self.main_frame))
         self.changePageButton.pack()
 
         # ホーム画面表示
@@ -48,10 +53,10 @@ class App(tk.Tk):
             print(self.field)
             # ゲーム画面へ
             if msg == "0":
-                print(msg,0)
+                print(msg, 0)
                 self.req()
             else:
-                print(msg,1)
+                print(msg, 1)
                 self.rec()
         except Exception as e:
             print(e)
@@ -62,10 +67,12 @@ class App(tk.Tk):
         self.rec_frame = tk.Frame()
         self.rec_frame.grid(row=0, column=0, sticky="nsew")
         # タイトル作成
-        self.titleLabel = tk.Label(self.rec_frame, text=f"{self.field}", font=('Helvetica', '35'))
+        self.titleLabel = tk.Label(
+            self.rec_frame, text=f"{self.field}", font=('Helvetica', '35'))
         self.titleLabel.pack(anchor='center', expand=True)
         # タイトル作成
-        self.titleLabel = tk.Label(self.rec_frame, text=f"相手の行動を待っています。", font=('Helvetica', '35'))
+        self.titleLabel = tk.Label(
+            self.rec_frame, text=f"相手の行動を待っています。", font=('Helvetica', '35'))
         self.titleLabel.pack(anchor='center', expand=True)
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -79,19 +86,24 @@ class App(tk.Tk):
         self.req_frame = tk.Frame()
         self.req_frame.grid(row=0, column=0, sticky="nsew")
         # タイトル作成
-        self.titleLabel = tk.Label(self.req_frame, text=f"{self.field}", font=('Helvetica', '35'))
+        self.titleLabel = tk.Label(
+            self.req_frame, text=f"{self.field}", font=('Helvetica', '35'))
         self.titleLabel.pack(anchor='center', expand=True)
         # 移動ボタン
-        self.upButtun = tk.Button(self.req_frame, text="↑", command=lambda : self.send("u"))
+        self.upButtun = tk.Button(
+            self.req_frame, text="↑", command=lambda: self.send("u"))
         self.upButtun.pack()
-        self.downButtun = tk.Button(self.req_frame, text="↓", command=lambda : self.send("d"))
+        self.downButtun = tk.Button(
+            self.req_frame, text="↓", command=lambda: self.send("d"))
         self.downButtun.pack()
-        self.rightButtun = tk.Button(self.req_frame, text="→", command=lambda : self.send("r"))
+        self.rightButtun = tk.Button(
+            self.req_frame, text="→", command=lambda: self.send("r"))
         self.rightButtun.pack()
-        self.leftButtun = tk.Button(self.req_frame, text="←", command=lambda : self.send("l"))
+        self.leftButtun = tk.Button(
+            self.req_frame, text="←", command=lambda: self.send("l"))
         self.leftButtun.pack()
 
-    def send(self,comand):
+    def send(self, comand):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.host, self.port))
         msg = self.s.send(f"{comand}".encode('ascii'))
